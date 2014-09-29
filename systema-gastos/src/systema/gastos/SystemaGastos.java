9202ae747b84e7587baec42a6a8e60359a9f0edf
alejandro-ipchile <alejandro.pizarro39972@edu.ipchile.cl>
2014-09-29 19:45:01 -0400

    cambio hecho por alejandro

--- systema-gastos/src/systema/gastos/SystemaGastos.java
+++ systema-gastos/src/systema/gastos/SystemaGastos.java
@@ -14,6 +14,7 @@ public class SystemaGastos {
      * @param args the command line arguments
      */
     // es te es un comentario para probar el git
+    // esto es un comentario echo por alejandro
     public static void main(String[] args) {
         // TODO code application logic here
     }
