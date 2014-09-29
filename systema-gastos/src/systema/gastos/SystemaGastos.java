f9a96fffa1f5a4a7555dc1c8a6514940daa5c99d
herman vargas <hvargas@betatest.cl>
2014-09-29 19:06:29 -0400

    agregue un comentario

--- systema-gastos/src/systema/gastos/SystemaGastos.java
+++ systema-gastos/src/systema/gastos/SystemaGastos.java
@@ -13,6 +13,7 @@ public class SystemaGastos {
     /**
      * @param args the command line arguments
      */
+    // es te es un comentario para probar el git
     public static void main(String[] args) {
         // TODO code application logic here
     }
