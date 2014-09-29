88fe8a881770773a792b94cdec7fc246efcdb112
herman vargas <hvargas@betatest.cl>
2014-09-29 18:59:02 -0400

    primer commit

---
+++ systema-gastos/src/systema/gastos/SystemaGastos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package systema.gastos;
+
+/**
+ *
+ * @author Profesor
+ */
+public class SystemaGastos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
