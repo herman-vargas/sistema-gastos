d50be3d21e48d00966a884c25867d1453b275f57
Laboratorio <Laboratorio@LAB2-PC20>
2014-09-30 21:11:37 -0400

    Agregue Forms y Clases de Usuario, Depto, Edificio y Jornadas de Conserjes Cambio de usuario

--- systema-gastos/src/clases/Usuario.java
+++ systema-gastos/src/clases/Usuario.java
@@ -1,6 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package clases;
@@ -10,19 +9,27 @@ package clases;
  * @author Laboratorio
  */
 public class Usuario {
-    private int rut;
-    private String nombre;
-    private String apellido;
-    private int sueldo;
-    private String perfil;
 
-    public Usuario(int rut, String nombre, String apellido, int sueldo, String perfil) {
-        this.rut = rut;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.sueldo = sueldo;
-        this.perfil = perfil;
+    public Usuario() {
     }
     
-    public Usuario(){}
+    
+ private String RutUsuario;
+ private String NombreUsuario;
+ private String ApellidoUsuario;
+ private int    SueldoUsuario;
+ private String PerfilUsuario;
+ 
+   
+   private void CrearUsuario(){      
+  }  
+   private void EliminarUsuario(){
+  } 
+   private void ModificarUsuario(){  
+  }  
+   private void BuscarUsuario(){  
+  }  
+   
+   
+    
 }
