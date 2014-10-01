7d8c36c799fd140d72fa7a7584f660790caeae08
Laboratorio <Laboratorio@LAB2-PC21>
2014-09-30 20:24:23 -0400

    creamos clase lavanderia

--- systema-gastos/src/clases/Lavanderia.java
+++ systema-gastos/src/clases/Lavanderia.java
@@ -12,7 +12,49 @@ import java.util.Date;
  * @author Laboratorio
  */
 public class Lavanderia {
-    private int cod;
+    
+    private int cod_Lav;
+    private int codDepto;
     private Date fecha;
-    private
+    private int valor;
+
+    public Lavanderia() {
+    }
+
+    public int getCod_Lav() {
+        return cod_Lav;
+    }
+
+    public void setCod_Lav(int cod_Lav) {
+        this.cod_Lav = cod_Lav;
+    }
+
+    public int getCodDepto() {
+        return codDepto;
+    }
+
+    public void setCodDepto(int codDepto) {
+        this.codDepto = codDepto;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+    public void Sumar_total_lavados ()   {
 }
+    }
+
+        
